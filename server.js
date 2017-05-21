520f98004622d78a7308fec8ad65b109b971b634
Helina <helina.bajracharya@gmail.com>
2017-05-21 16:45:01 +0000

    timestamp

--- server.js
+++ server.js
@@ -1,8 +1,29 @@
 var express = require('express')
+var moment = require('moment');
+
 var app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+app.get('/:datestring', function (req, res) {
+  
+  var date=req.params.datestring;
+  
+  var json = {
+    unix: null,
+    natural: null
+  };
+  
+  var newdate;
+  
+  if(/^\d{8,}$/.test(date)) newdate = moment(date, "X");
+  else newdate = moment(date, "MMMM D, YYYY");
+  
+  if(newdate.isValid()) {
+    json.unix=newdate.format("X");
+    json.natural=newdate.format("MMMM D, YYYY");
+  }
+  
+  res.send(json);
+ 
 })
 
 app.listen(8080, function () {
